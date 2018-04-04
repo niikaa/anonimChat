7e61ee45621bbe6b9dd2e254e3b5aaa8751af881
nnikka <nikakhurashvili@gmail.com>
2018-04-04 21:45:48 +0400

    loader

---
+++ src/features/components/Loaders/FullLoader.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="full-loader-container">
+    <div class="vert-mid">
+      <div class="lds-css-full">
+        <div class="lds-facebook vert-mid filled">
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+
+  }
+</script>
