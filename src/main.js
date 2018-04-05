3089975b54d8fa92fd04b8a4779110053f266889
nnikka <nikakhurashvili@gmail.com>
2018-04-05 20:10:40 +0400

    add vuetify && vue-resource cfgs

--- src/main.js
+++ src/main.js
@@ -3,10 +3,25 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import Vuetify from 'vuetify'
+import VueResource from 'vue-resource'
+
+Vue.use(VueResource);
+
+Vue.use(Vuetify, {
+  theme: {
+    primary: '#3498db',
+    secondary: '#ecf0f1',
+    accent: '#82B1FF',
+    error: '#FF5252',
+    info: '#2196F3',
+    success: '#4CAF50',
+    warning: '#FFC107'
+  }
+})
 
 Vue.config.productionTip = false
 
-/* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
