8fa5ae77277bf11769f46e9cfa71236ba87c6865
nnikka <nikakhurashvili@gmail.com>
2018-04-05 09:58:59 +0400

    welcome page

--- src/features/welcome/index.vue
+++ src/features/welcome/index.vue
@@ -1,6 +1,29 @@
 <template>
   <div class="welcome-pg">
-
+    <div class="ann-container">
+      <div class="row">
+        <div class="col span-6-of-12">
+          <h2>
+            Opinions do matter, use them to build yourself!
+          </h2>
+          <span class="under-h2">
+            Are your friends really on your side? Get honest feedbacks and know what you don’t!
+          </span>
+        </div>
+        <div class="col span-6-of-12">
+          <div class="ann-segment">
+            <h4>
+              Sign up or Login with facebook
+            </h4>
+            <div class="ann-centered">
+              <button class="btn-lg">
+                Sign up
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
