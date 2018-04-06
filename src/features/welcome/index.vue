f608706ba3d0232b8b090bef16adb4b852270e5f
nnikka <nikakhurashvili@gmail.com>
2018-04-06 12:22:49 +0400

    welcome page refactor

--- src/features/welcome/index.vue
+++ src/features/welcome/index.vue
@@ -1,29 +1,32 @@
 <template>
-  <div class="welcome-pg">
-    <div class="ann-container">
-      <div class="row">
-        <div class="col span-6-of-12">
-          <h2>
+  <div class="home-pg">
+    <v-container class="container-toped">
+      <v-layout row wrap class="main-pad">
+        <v-flex xs12 md6>
+          <h1 class="ann-h1">
             Opinions do matter, use them to build yourself!
-          </h2>
-          <span class="under-h2">
+          </h1>
+          <span class="ann-under-h1">
             Are your friends really on your side? Get honest feedbacks and know what you don’t!
           </span>
-        </div>
-        <div class="col span-6-of-12">
-          <div class="ann-segment">
-            <h4>
-              Sign up or Login with facebook
-            </h4>
-            <div class="ann-centered">
-              <button class="btn-lg">
-                Sign up
-              </button>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+        </v-flex>
+        <v-flex xs12 md6>
+          <v-card class="white--text auth-card">
+            <v-card-text class="px-100">
+              <h4 class="ann-h4">
+                Sign up or Login with facebook
+              </h4>
+              <div class="ann-centered">
+                <div class="text-xs-center">
+                  <v-btn round large color="light-blue darken-1">Log in</v-btn>
+                  <!--<v-btn round large color="light-blue darken-1">Log out</v-btn>-->
+                </div>
+              </div>
+            </v-card-text>
+          </v-card>
+        </v-flex>
+      </v-layout>
+    </v-container>
   </div>
 </template>
 
@@ -32,7 +35,3 @@ export default {
 
 }
 </script>
-
-<style>
-
-</style>
