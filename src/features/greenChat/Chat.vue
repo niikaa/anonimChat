1a3c8a0f6076ed77de144f17d81293384a7b49e6
nnikka <nikakhurashvili@gmail.com>
2018-04-08 17:15:04 +0400

    responsive chat layout, + chat view

---
+++ src/features/greenChat/Chat.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="fill-area">
+    <div class="subheader dark-text">
+      Friend info
+    </div>
+    <hr class="divider light-background">
+    <div class="chat-container">
+      <v-layout row>
+        <v-flex xs-12>
+          <div class="chat-my-msg">
+            Gamarjoba
+          </div>
+        </v-flex>
+      </v-layout>
+      <v-layout row>
+        <v-flex xs-12>
+          <div class="chat-partner-msg">
+            Gamarjoba
+          </div>
+        </v-flex>
+      </v-layout><v-layout row>
+        <v-flex xs-12>
+          <div class="chat-my-msg">
+            Gamarjoba
+          </div>
+        </v-flex>
+      </v-layout>
+      <v-layout row>
+        <v-flex xs-12>
+          <div class="chat-partner-msg">
+            Gamarjoba
+          </div>
+        </v-flex>
+      </v-layout>
+    </div>
+    <div class="bottom-line">
+      <form>
+        <v-layout row>
+          <v-flex xs-12>
+            <input type="text" class="chat-input" >
+            <div class="overed-rows">
+              <v-btn flat icon color="blue">
+                <v-icon>send</v-icon>
+              </v-btn>
+            </div>
+          </v-flex>
+        </v-layout>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
--- src/features/greenChat/index.vue
+++ src/features/greenChat/index.vue
@@ -1,14 +1,19 @@
 <template>
   <v-container>
     <AppFriendsList></AppFriendsList>
+    <div class="middle-fixed-container">
+      <AppChat></AppChat>
+    </div>
   </v-container>
 </template>
 
 <script>
 import FriendsList from './FriendsList'
+import Chat from './Chat'
 export default {
   components: {
     AppFriendsList: FriendsList,
+    AppChat: Chat,
   }
 }
 </script>
