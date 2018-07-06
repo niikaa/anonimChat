90b2525fc9eea94891820a457345c0ba9616908c
omari <oshek14@freeuni.edu.ge>
2018-07-06 16:32:21 +0400

    chat fixed finally

--- server/server.js
+++ server/server.js
@@ -27,6 +27,7 @@ io.on('connection', function(socket){
     connections.removeConnection({...data})
   })
   socket.on('SEND_GREEN_CHAT_MESSAGE', (data) => {
+
     data.message.conversation_id = data.conversation_id
     data.initTargets.forEach(element => {
         io.to(element.socket_id).emit('GREEN_CHAT_MSG_RECEIVE', data.message)
@@ -36,6 +37,11 @@ io.on('connection', function(socket){
     })
   })
   socket.on('SEND_BLUE_CHAT_MESSAGE', (data) => {
+    console.log("===============================================")
+    console.log(data.initTargets);
+    console.log(data.targetTargets);
+    console.log("===============================================")
+    
     data.message.conversation_id = data.conversation_id
     data.initTargets.forEach(element => {
         io.to(element.socket_id).emit('BLUE_CHAT_MSG_RECEIVE', data.message)
@@ -45,7 +51,12 @@ io.on('connection', function(socket){
     })
   })
   socket.on('SEND_RED_CHAT_MESSAGE', (data) => {
-    data.message.conversation_id = data.conversation_id
+      console.log("===============================================")
+      console.log(data.initTargets);
+      console.log(data.targetTargets);
+      console.log("===============================================")
+
+      data.message.conversation_id = data.conversation_id
     data.initTargets.forEach(element => {
         io.to(element.socket_id).emit('RED_CHAT_MSG_RECEIVE', data.message)
     })
