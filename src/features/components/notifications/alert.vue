bc86c4838d4ec2cc89a59c10d55c81e909c2144a
novaknole <nikakhurashvili@gmail.com>
2018-07-15 15:50:28 +0400

    add notification components

---
+++ src/features/components/notifications/alert.vue
@@ -0,0 +1,39 @@
+<template>
+  <div :ref="'not-' + id">
+    <v-alert
+      class="alert-grass"
+      :value="true"
+      :color="color + ' lighten-1'"
+      transition="scale-transition"
+    >
+      <v-avatar>
+        <img
+          :src="img  ? img : '/static/icons/bgender.png'"
+        >
+      </v-avatar>
+      <div class="not-txt-cont">
+        <h3>{{header}}</h3>
+        <p class="not-sub-header">
+          {{text}}
+        </p>
+      </div>
+    </v-alert>
+  </div>
+</template>
+
+<script>
+export default {
+  props:['text', 'img', 'header', 'id', 'color'],
+  data() {
+    return {
+
+    }
+  },
+  computed: {
+
+  },
+  methods: {
+
+  }
+}
+</script>
